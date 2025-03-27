1ec7919f81457dd4ffc9d3fa66ec246e4cae4f25
Sophia09-Pixel <sophia.de0903@gmail.com>
2025-03-26 22:11:27 -0300

    atividades

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { FirstComponentComponent } from "./components/first-component/first-component.component";
+import { SecondComponentComponent } from "./components/second-component/second-component.component";
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FirstComponentComponent, SecondComponentComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
